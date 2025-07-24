f987caf75c4ebd0ef1b1cd97cf6b5db0c5358700
chaeyn <chaulfe@gmail.com>
2025-07-24 17:42:03 +0900

    todo-list

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState<string[]>([]);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setToDo(event.target.value);
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    console.log(toDos);
+  };
+  return (
+    <>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do "
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
